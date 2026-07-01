d96ecfaf2bae48324bc655cff0f3aa481f7740d8
tai nguyen <ntnguyenscomvn@gmail.com>
2026-07-01 20:22:11 +0700

    update source 1

--- src/shoesShop/modalBoxContent.jsx
+++ src/shoesShop/modalBoxContent.jsx
@@ -8,13 +8,86 @@ export default function ModalBoxContent(props) {
     >
       <div className="relative w-full max-w-7xl max-h-full">
         {/* Modal content */}
-        <div className="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
+        <div className="relative md:static bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
           {/* Modal header */}
-          <div className="flex items-center justify-between border-b border-default pb-4 md:pb-5">
+          <div className="relative flex items-center flex-col md:flex-row justify-between gap-5 md:gap-10 border-b border-default pb-4 md:pb-5">
             <h3 className="text-lg font-medium text-heading">Product Detail</h3>
+            {/* Modal Breadcrumb */}
+            <nav className="flex" aria-label="Breadcrumb">
+              <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li className="inline-flex items-center">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    <svg
+                      className="me-2.5 h-3 w-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                    </svg>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <svg
+                      className="h-5 w-5 text-gray-400 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={24}
+                      height={24}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m9 5 7 7-7 7"
+                      />
+                    </svg>
+                    <a
+                      href="#"
+                      className="ms-1 text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white md:ms-2"
+                    >
+                      Products
+                    </a>
+                  </div>
+                </li>
+                <li aria-current="page">
+                  <div className="flex items-center">
+                    <svg
+                      className="h-5 w-5 text-gray-400 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={24}
+                      height={24}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m9 5 7 7-7 7"
+                      />
+                    </svg>
+                    <span className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">
+                      {getItem && getItem.alias}
+                    </span>
+                  </div>
+                </li>
+              </ol>
+            </nav>
             <button
               type="button"
-              className="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center"
+              className="absolute top-0 right-0 md:static text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center"
               data-modal-hide="quickView-modal"
             >
               <svg
@@ -181,13 +254,13 @@ export default function ModalBoxContent(props) {
                       </svg>
                       Add to cart
                     </a>
-                    <div className="flex items-center justify-between gap-2">
+                    <div className="mt-4 sm:mt-0 flex items-center justify-start sm:justify-between gap-2">
                       <span className="text-fg-brand text-sm font-medium leading-5 text-gray-800 flex items-center gap-1">
                         <svg
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
-                          width={20}
-                          height={20}
+                          width={18}
+                          height={18}
                           fill="currentColor"
                           viewBox="0 0 24 24"
                         >
